8d03d954b18fb67b293daf76a36d62a2cd2879ae
kaponero <kaponero@gmail.com>
2024-01-02 22:05:11 -0300

    Iniciando el login

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,11 +1,15 @@
 from flask import render_template, redirect, request, url_for, flash, session
 
+from app import tryton
 from app.auth import blueprint
 from app.base import blueprint as base_blueprint
 
 from .forms import LoginForm
 
 from functools import wraps
+
+WebUser = tryton.pool.get('web.user')
+Session = tryton.pool.get('web.user.session')
 
 """
 def login_required(func):
@@ -19,12 +23,11 @@ def login_required(func):
             return redirect(url_for('auth_blueprint.login', next=request.path))
         return func(*args, **kwargs)
     return wrapper
-
+"""
 @blueprint.route('/login', methods=['GET', 'POST'])
 @tryton.transaction()
 def login():
-    Jury = tryton.pool.get('aet_web.jury')
-    Invitation = tryton.pool.get('aet_web.invitation')
+    Party = tryton.pool.get('party.party')
     login_form = LoginForm(request.form)
     if 'login' in request.form:
         #request.method == 'POST' and login_form.validate_on_submit():
@@ -34,22 +37,13 @@ def login():
             if webuser:
                 session['session_key'] = WebUser.new_session(webuser)
                 session['identified'] = True
-                ''' Si el usuario es jurado, regresamos la lista de categorias '''
-                jury = Jury.search([('web_user', '=', webuser)])
-                invitation = Invitation.search([('web_user', '=', webuser)])
-                if jury and invitation:
-                    return redirect(url_for('base_blueprint.lista_o_reserva'))
-                if jury:
-                    return redirect(url_for('base_blueprint.show_category'))
-                ''' Si el usuario es para comprar invitaciones, regresamos a la parte de invitaciones'''
-                if invitation:
-                    return redirect(url_for('base_blueprint.listado_reserva'))
+                return redirect(url_for('base_blueprint.index'))
             flash('Verifique sus credenciales', 'error')
             return render_template( 'login.html', msg='Correo electrónico o contraseña incorrecta', form=login_form)
         except:
             return 'Demasiados intentos de ingreso o algun otro error. Espere un momento y vuelvalo a intentar'
     return render_template('login.html', form=login_form)
-
+"""
 # Logout
 @login_required
 @blueprint.route('/logout')
