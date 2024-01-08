c327fbfca783cb8add66146d4964d9e96a4796ea
kaponero <kaponero@gmail.com>
2024-01-08 18:23:04 -0300

    Iniciando menu del dashboard

--- app/base/static/asset/js/scriptjurados.js
+++
@@ -1,7 +0,0 @@
-function recuperar_calificacion(identificador,valor_calificacion){
-	
-    var select= document.getElementById(identificador);
-    if(valor_calificacion != null){
-        select.selectedIndex = valor_calificacion;
-    }
-}
