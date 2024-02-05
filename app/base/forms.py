9b2b5d8016e1c6f1ef1c599ed735afa5a43bf65e
Francisco Moyano <moyanocasco.franciscom@gmail.com>
2024-02-05 10:51:45 -0300

    nereida_portal: se configura el endpoint de cambio de contraseña y otras pequeñas
    modificaciones al template de seguridad

---
+++ app/base/forms.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+
+from flask_wtf import FlaskForm
+from wtforms import PasswordField
+from wtforms.validators import DataRequired
+
+
+class ChangePasswordForm(FlaskForm):
+    password = PasswordField('Password', id='pwd_login',
+            validators=[DataRequired()])
+    password_confirmation = PasswordField('Password Confirmation', id='pwd_login',
+            validators=[DataRequired()])
--- app/base/routes.py
+++ app/base/routes.py
@@ -2,10 +2,17 @@ from flask import (render_template, redirect, request, url_for,
             flash, session, send_file, jsonify)
 
 from app import tryton
+
+from .forms import ChangePasswordForm
+
 from app.base import blueprint
+
 from app.auth.routes import login_required
+
 from app.download_attachment.routes import download_report
+
 from trytond.transaction import Transaction
+
 
 @blueprint.route('/index')
 @tryton.transaction()
@@ -49,13 +56,13 @@ def perfil():
     Subscriptor = tryton.pool.get('delco.subscriptor')
     Session = tryton.pool.get('web.user.session')
     user = Session.get_user(session['session_key'])
-    
+
     if Subscriptor.search([('web_user', '=', user)]):
         with Transaction().set_context(company=1):
             subscriptor, = Subscriptor.search([('web_user', '=', user)])
             return render_template('/perfil.html', subscriptor=subscriptor)
     return render_template('page-500.html'), 500
-    
+
 
 @blueprint.route('/notificaciones')
 @tryton.transaction()
@@ -64,17 +71,27 @@ def notificaciones():
     return render_template('/notificaciones.html')
 
 
-@blueprint.route('/seguridad')
+@blueprint.route('/seguridad', methods = ['GET','POST'])
 @tryton.transaction()
 @login_required
 def seguridad():
-    Subscriptor = tryton.pool.get('delco.subscriptor')
+    WebUser = tryton.pool.get('web.user')
     Session = tryton.pool.get('web.user.session')
+    Subscriptor = tryton.pool.get('delco.subscriptor')
+    change_password_form = ChangePasswordForm(request.form)
     user = Session.get_user(session['session_key'])
-    
+    web_user = WebUser(user)
     if Subscriptor.search([('web_user', '=', user)]):
         with Transaction().set_context(company=1):
             subscriptor, = Subscriptor.search([('web_user', '=', user)])
-            return render_template('/seguridad.html', subscriptor=subscriptor)
+            if request.method == 'POST' \
+                and change_password_form.password.data == change_password_form.password_confirmation.data:
+                # aquí el cambio de contraseña por tryton
+                flash('La contraseña ha sido exitosamente actualizada', 'success')
+                web_user.password = change_password_form.password.data
+                web_user.save()
+            elif request.method == 'POST':
+                flash('Las contraseñas no coinciden', 'error')
+            return render_template('/seguridad.html', subscriptor=subscriptor,
+                    form=change_password_form)
     return render_template('page-500.html'), 500
-    
