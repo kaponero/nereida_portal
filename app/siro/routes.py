0b70c777534206381fa7b59f6b0e732ec4b57cb3
Francisco Moyano <moyanocasco.franciscom@gmail.com>
2025-05-16 12:29:08 -0300

    nereida_portal: se agrega el log de la transacción a endpoint siro_(no_)success

--- app/siro/routes.py
+++ app/siro/routes.py
@@ -2,13 +2,15 @@ from app import tryton
 from app.siro import blueprint
 from app.auth.routes import login_required
 
-from flask import render_template, request, Response, jsonify, abort
+from flask import render_template, request, Response, jsonify, abort, make_response
 
+from trytond.transaction import Transaction
 from datetime import datetime
+from decimal import Decimal
 
 import qrcode
 import io
-
+import json
 import hashlib
 import hmac
 import os
@@ -21,6 +23,13 @@ def generar_hmac(id_referencia):
     message = f"{id_referencia}{SECRET_KEY}".encode()
     return hmac.new(SECRET_KEY.encode(), message, hashlib.sha256).hexdigest()
 
+def log_response():
+    response_data = {"status": "success"}
+    response = make_response(json.dumps(response_data))
+    raw_response = f"HTTP {response.status_code}\n"
+    raw_response += "\n".join([f"{k}: {v}" for k, v in response.headers.items()])
+    raw_response += f"\n\n{response.data.decode('utf-8')}"
+    return raw_response
 
 @blueprint.route('/siro_button/<voucher_id>')
 @tryton.transaction()
@@ -49,27 +58,46 @@ def pay(voucher_id):
 @tryton.transaction(readonly=False, user=2)
 @login_required
 def generate_qr(voucher_id):
-    print(20*'*', 'Comienzo de endpoint generate_qr', 20*'*')
-    try:
-        pool = tryton.pool
-        Voucher = pool.get('delco.subscriptor.voucher')
+    pool = tryton.pool
+    Voucher = pool.get('delco.subscriptor.voucher')
+    Log = pool.get('delco.siro.log')
 
+    # Comenzamos con el log
+    log = Log.log_request(request)
+    log.raw_response = log_response()
+    log.date = datetime.now()
+    log.status = 'generating'
+    log.save()
+
+    try:
         voucher_id = request.args.get('lineId', type=int)
         amount_to_pay_today = request.args.get('amount_to_pay_today', type=float)
+        log.amount = Decimal(amount_to_pay_today)
+        log.save()
 
         # Obtener el voucher por su ID
         voucher, = Voucher.search([('id', '=', voucher_id)])
+
+        log.voucher = voucher
+        log.subscriptor = log.on_change_with_subscriptor()
+        log.save()
 
         qr = Voucher.generate_siro_qrs([{
             'voucher': voucher,
             'amount_to_pay_today': amount_to_pay_today
             }])
-        print('luego del fetch')
         if qr:
             # Asignar los valores al voucher
             voucher.siro_qr_string = qr[0]['StringQR']
             voucher.siro_qr_hash = qr[0]['Hash']
             voucher.save()  # Guardar los cambios en la base de datos
+
+            log.siro_qr_hash = voucher.siro_qr_hash
+            log.siro_IdReferenciaOperacion = voucher.siro_IdReferenciaOperacion
+            log.siro_hmac = voucher.siro_hmac
+            log.status = 'generated'
+            log.save()
+
             # Devolver una respuesta de éxito
             return jsonify({
                 "status": "success",
@@ -80,12 +108,18 @@ def generate_qr(voucher_id):
             }), 200
         else:
             # Devolver un error si no se obtuvieron los parámetros del QR
+            log.status = 'error'
+            log.error_message = "No se pudieron obtener los parámetros del QR"
+            log.save()
             return jsonify({
                 "status": "error",
                 "message": "No se pudieron obtener los parámetros del QR"
             }), 400
     except Exception as e:
         # Manejar cualquier excepción y devolver un error
+        log.status = 'error'
+        log.error_message = f"Error al generar el QR: {str(e)}"
+        log.save()
         return jsonify({
             "status": "error",
             "message": f"Error al generar el QR: {str(e)}"
@@ -126,6 +160,7 @@ def show_qr():
 def handle_success(voucher_id) :
     pool = tryton.pool
     Voucher = pool.get('delco.subscriptor.voucher')
+    Log = pool.get('delco.siro.log')
 
     data_requests = request.args
     print("Pago exitoso recibido: ", data_requests)
@@ -141,12 +176,19 @@ def handle_success(voucher_id) :
 
     id_resultado = request.args.get('IdResultado', None)
     print(request.args)
-    print(
-        id_referencia, id_referencia_tryton,
-        id_referencia==id_referencia_tryton, 
-        hmac_recibido, hmac_tryton,
-        hmac_recibido==hmac_tryton,
-        )
+
+    # Comenzamos con el log
+    log = Log.log_request(request)
+    log.raw_response = log_response()
+    log.voucher = voucher.id
+    log.subscriptor = log.on_change_with_subscriptor()
+    log.date = datetime.now()
+    log.status = 'pending'
+    log.siro_qr_hash = hmac_recibido
+    log.siro_IdReferenciaOperacion = id_referencia
+    log.siro_IdResultado = id_resultado
+    log.save()
+
     # Validación del hmac (para POST, o sea, la billetera)
     if  hmac_recibido and id_resultado \
         and hmac_recibido == hmac_tryton \
@@ -158,6 +200,9 @@ def handle_success(voucher_id) :
             generar_hmac(id_referencia),
             hmac_recibido
             ):
+            log.status = 'error'
+            log.error_message("403, Firma inválida")
+            log.save()
             print('abortado')
             abort(403, "Firma inválida")
 
@@ -176,6 +221,13 @@ def handle_success(voucher_id) :
 
         if voucher.state == 'processing_payment':
             Voucher.check_siro_payments([voucher])
+        if voucher.state == 'process_payment_ok':
+            log.status = 'paid'
+            log.save()
+        else:
+            log.status = 'error'
+            log.error_message = "No se pudo concretar la corroboración del pago"
+            log.save()
 
         return jsonify({"status": "OK"}), 200
 
@@ -184,6 +236,8 @@ def handle_success(voucher_id) :
         Redirección desde la página del QR
         '''
         print('pago exitoso')
+        log.status = "Redirecting OK"
+        log.save()
         return render_template('/siro-pago-exitoso.html',
             subscriptor=subscriptor, voucher=voucher)
 
@@ -193,10 +247,22 @@ def handle_success(voucher_id) :
 def siro_no_success(voucher_id) :
     pool = tryton.pool
     Voucher = pool.get('delco.subscriptor.voucher')
+    Log = pool.get('delco.siro.log')
+
     voucher = Voucher(voucher_id)
 
     hmac_recibido = request.args.get('hmac', None)
     id_referencia = voucher.siro_IdReferenciaOperacion
+
+    log = Log.log_request(request)
+    log.raw_response = log_response()
+    log.voucher = voucher.id
+    log.subscriptor = log.on_change_with_subscriptor()
+    log.date = datetime.now()
+    log.status = 'error'
+    log.siro_qr_hash = hmac_recibido
+    log.siro_IdReferenciaOperacion = id_referencia
+    log.save()
 
     if hmac_recibido:
         """
@@ -211,7 +277,8 @@ def siro_no_success(voucher_id) :
             ):
             print('abortado')
             abort(403, "Firma inválida")
-
+        log.error_message = "Pago no exitoso. La billetera pega en URL NO OK"
+        log.save()
         voucher.state = 'process_payment_fail'
         voucher.save()
         return jsonify({"status": "OK"}), 200
@@ -219,6 +286,10 @@ def siro_no_success(voucher_id) :
         """
         Redirección desde la página del QR
         """
+        log.error_message = \
+            "Pago no exitoso. La billetera pega en URL NO OK. " \
+            + " Redireccionamiento OK"
+        log.save()
         subscriptor = voucher.subscriptor
         return render_template('/siro-pago-no-exitoso.html',
             subscriptor=subscriptor, voucher=voucher)
@@ -247,6 +318,6 @@ def verficar_pago():
     else:
         estado = 'EN ESPERA'
 
-    if voucher.state in ['processing_payment', 'process_payment_ok']:
+    if voucher.state in ['processing_payment', 'process_payment_ok', 'paid']:
         estado = "APROBADO"
     return jsonify({"estado": estado}), 200
