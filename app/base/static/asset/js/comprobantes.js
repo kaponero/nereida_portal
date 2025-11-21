3fb04b828d70da655bfb975e9c630633debf0b16
Alvaro <kaponero@gmail.com>
2025-11-21 20:12:33 -0300

    Cambios en tabla comprobantes

---
+++ app/base/static/asset/js/comprobantes.js
@@ -0,0 +1,28 @@
+
+    $(document).ready(function() {
+        $('#tabla_comprobantes').DataTable({
+            "pageLength": 10,
+            "lengthMenu": [10, 25, 50, 100],
+            "language": {
+                "decimal": "",
+                "emptyTable": "No hay datos disponibles",
+                "info": "Mostrando _START_ a _END_ de _TOTAL_ registros",
+                "infoEmpty": "Mostrando 0 a 0 de 0 registros",
+                "infoFiltered": "(filtrado de _MAX_ registros totales)",
+                "infoPostFix": "",
+                "thousands": ",",
+                "lengthMenu": "Mostrar _MENU_ registros",
+                "loadingRecords": "Cargando...",
+                "processing": "Procesando...",
+                "search": "Buscar:",
+                "zeroRecords": "No se encontraron resultados",
+                "paginate": {
+                    "first": "Primero",
+                    "last": "Último",
+                    "next": "Siguiente",
+                    "previous": "Anterior"
+                }
+            }
+        });
+    });
+  
