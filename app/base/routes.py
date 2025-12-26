34a664badd0540e7b0a883e1f6bcdeb23e088335
Alvaro <kaponero@gmail.com>
2025-12-26 12:53:34 -0300

    cargar equipos de tryton

--- app/base/routes.py
+++ app/base/routes.py
@@ -180,10 +180,18 @@ def get_multipago_qr(subscriptor_id):
 def equipos():
     Subscriptor = tryton.pool.get('delco.subscriptor')
     Session = tryton.pool.get('web.user.session')
+
     user = Session.get_user(session['session_key'])
-    if Subscriptor.search([('web_user', '=', user)]):
-        with Transaction().set_context(company=1):
-            subscriptor, = Subscriptor.search([('web_user', '=', user)])
-            return render_template('/equipos.html', subscriptor=subscriptor)
-    return render_template('page-500.html'), 500
-    
+
+    subs = Subscriptor.search([('web_user', '=', user)])
+    if not subs:
+        return jsonify({'error': 'Subscriptor no encontrado'}), 404
+
+    with Transaction().set_context(company=1):
+        sub = subs[0]
+
+        return render_template(
+            'equipos.html',
+            subscriptor=sub,
+            equipos=sub.equipments
+        )
